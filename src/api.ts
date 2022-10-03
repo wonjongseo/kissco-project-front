c41878488a6b1f0f71f30dc9bde9dc58aa6de1fb
wonjongseo <visionwill@naver.com>
2022-10-03 19:48:56 +0900

    change base url

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-// export const BASE_URL = "https://kissco-dic.herokuapp.com";
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://kissco-dic.herokuapp.com";
+// export const BASE_URL = "http://localhost:8080";
 
 export const searchWord = async (word: string) => {
   const new_url = `${BASE_URL}/api/vocas`;
